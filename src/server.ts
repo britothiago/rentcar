0343dcd854b8c1941899c0c485c824d737231fc7
thiagodevbr <britotr@gmail.com>
2022-08-27 23:53:51 -0300

    Add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { healthcheckRoutes } from "./routes/healthcheck.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
 
+import swaggerFile from "./swagger.json";
+
 const app = express();
 app.use(express.json());
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/categories", categoriesRoutes);
 app.use("/specifications", specificationsRoutes);
